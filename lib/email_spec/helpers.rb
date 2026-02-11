ab554c03fa21df54ec3b17226ed56f0bd8c71438
Matt Jankowski <matt@jankowski.online>
2026-02-11 12:55:34 -0500

    Fix `Style/SlicingWithRange` cop in helpers

--- lib/email_spec/helpers.rb
+++ lib/email_spec/helpers.rb
@@ -145,7 +145,7 @@ module EmailSpec
     # e.g. Click here in  <a href="http://confirm">Click here</a>
     def parse_email_for_anchor_text_link(email, link_text)
       if textify_images(email.default_part_body) =~ %r{#{A_TAG_BEGIN_REGEX}#{link_text}#{A_TAG_END_REGEX}}
-        URI.split($1)[5..-1].compact!.join("?").gsub("&amp;", "&")
+        URI.split($1)[5..].compact!.join("?").gsub("&amp;", "&")
         # sub correct ampersand after rails switches it (http://dev.rubyonrails.org/ticket/4002)
       end
     end
